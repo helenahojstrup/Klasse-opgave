cd9fc59642ac00655159e03ecb1b90e773d37745
helenahojstrup <fodboldhelena4@gmail.com>
2026-09-09 09:28:39 +0200

    indsætning af sektioner

---
+++ index.js
@@ -0,0 +1,38 @@
+console.log('Happy developing ✨')
+<< !doctype
+html >
+< html
+lang = "en" >
+    < head >
+    < meta
+charset = "UTF-8" >
+    < meta
+name = "viewport"
+content = "width=device-width, initial-scale=1" >
+    < title > Document < /title>
+</head>
+<body>
+<section class="Thomas">
+    <p>
+
+    </p>
+</section>
+
+<section class="Ezgi">
+    <p>
+
+    </p>
+    </section>
+<section class="Omar">
+    <p>
+
+    </p>
+</section>
+<section class="Helena">
+    <p>
+
+    </p>
+</section>
+
+</body>
+</html>
